663dfdbb66ba4be4c9374de45d79dbb5f9ec71c0
AdrianGarridoPantaleon <adrian.garrido01@goumh.umh.es>
2023-04-19 12:37:22 +0200

    Reto#1

---
+++ C/Reto#0.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            printf("%d- fizzbuzz\n",i);
+        }
+        else if (i % 5 == 0)
+        {
+            printf("%d- buzz\n",i);
+        }
+        else if (i % 3 == 0)
+        {
+            printf("%d- fizz\n",i);
+        }
+    }
+}
